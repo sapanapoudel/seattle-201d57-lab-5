b50f9627a022554a75b4a308040fdf672abaf044
sapanapoudel <sapana.poudel17@outlook.com>
2019-06-08 16:20:06 -0700

    sumAndMultiply

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,9 +10,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  console.log('a : ' + a);
-  console.log('b : ' + b);
-
   var total = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + total + '.';
 
@@ -20,7 +17,7 @@ function sum(a, b) { //eslint-disable-line
 }
 //Here is the test for sum(); uncomment it to run it
 // eslint-disable-next-line no-undef
-//testSum(4, 7);
+testSum(4, 7);
 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -34,8 +31,6 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
   var answer = a * b;
-  console.log('a: ' + a);
-  console.log('b: ' + b);
   var message = 'The product of ' + a + ' and ' + b + ' is ' + answer + '.';
   return [answer, message];
 }
@@ -55,11 +50,19 @@ his function by hand in the console to get it working, and when you think it is 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var answer = [];
+  answer.push(sum(a, b)[0]);
+  answer.push(sum(answer.pop(), c)[0]);
  
+  answer.push(multiply(a, b)[0]);
+  answer.push(multiply(answer.pop(), c)[0]);
+  answer.push(a + ' and ' + b + ' and ' + c + ' sum to ' + answer[0] + '.');
+  answer.push('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + answer[1]+ '.');
+  return answer;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
